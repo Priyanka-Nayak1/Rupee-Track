f1fe41d5c15eab6962d0c1f5270b5edb710e57f8
Priyanka-Nayak1 <150772336+Priyanka-Nayak1@users.noreply.github.com>
2025-06-08 22:01:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv');
+const colors = require('colors');
+const connectDb = require('./config/connectDb');
+// const path = require('path');
+// config dotenv file
+dotenv.config();
+
+//database call
+connectDb();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use('/api/v1/users', require('./routes/userRoute'));
+//transactions routes
+app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
+
+// //static files
+// app.use(express.static(path.join(__dirname, './client/build')));
+
+// app.get('*', function(req,res){
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
